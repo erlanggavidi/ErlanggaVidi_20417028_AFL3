8c897d604a3f015b41cf244627d665bf4cc23948
deja232 <93458378+nextrektor@users.noreply.github.com>
2023-04-28 02:32:57 +0700

    Initial Commit

---
+++ ErlanggaVidi_20417028_AFL3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ErlanggaVidi_20417028_AFL3
+//
+//  Created by SIFT - Telkom DBT Air 8 on 28/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ErlanggaVidi_20417028_AFL3/ErlanggaVidi_20417028_AFL3App.swift
@@ -0,0 +1,17 @@
+//
+//  ErlanggaVidi_20417028_AFL3App.swift
+//  ErlanggaVidi_20417028_AFL3
+//
+//  Created by SIFT - Telkom DBT Air 8 on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ErlanggaVidi_20417028_AFL3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
