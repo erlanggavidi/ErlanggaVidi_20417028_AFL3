e3aaa15c91751768f1a2e4a4dffd8a72f6d0cf21
deja232 <93458378+nextrektor@users.noreply.github.com>
2023-04-28 02:49:39 +0700

    Section 3, Step 1 - 9

--- ErlanggaVidi_20417028_AFL3/ContentView.swift
+++ ErlanggaVidi_20417028_AFL3/ContentView.swift
@@ -1,16 +1,19 @@
-//
-//  ContentView.swift
-//  ErlanggaVidi_20417028_AFL3
-//
-//  Created by SIFT - Telkom DBT Air 8 on 28/04/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
     }
 }
 
